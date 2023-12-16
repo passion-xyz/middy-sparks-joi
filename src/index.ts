a4b71940c6c08e5ad2b12e4d13da6565072fc65e
Thant Sin Oo ( Howard Chang ) <drawoh@passion.xyz>
2023-12-16 19:31:10 +0800

    Validate by fields

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import createError from 'http-errors'
 import Joi, { Schema, ValidationResult } from 'joi'
 
@@ -10,29 +11,36 @@ export const middleware = ({
 	schema,
 	options,
 }: {
-	schema: Schema
+	schema: { [key: string]: Schema }
 	options?: any
 }) => {
-	if (!Joi.isSchema(schema)) {
-		console.log(
-			'[middy-sparks-joi] The schema you provided is not a valid Joi schema',
-		)
-		throw new Error('The schema is not valid')
-	}
+	_.map(_.keys(schema), (key: string) => {
+		if (!Joi.isSchema(schema[key])) {
+			console.log(
+				'[middy-sparks-joi] The schema you provided is not a valid Joi schema',
+			)
+			throw new Error('The schema is not valid')
+		}
+	})
 
 	return {
 		before: (handler: Handler, next: () => void) => {
-			const event = handler.event
-			const validation: ValidationResult = schema.validate(event, options)
-
-			if (validation.error) {
-				const error = new createError.BadRequest(
-					'Event object failed validation',
+			_.map(_.keys(schema), (key: string) => {
+				const input = handler.event[key]
+				const validation: ValidationResult = schema[key].validate(
+					input,
+					options,
 				)
-				handler.event.headers = { ...handler.event.headers }
-				error.details = validation.error.details
-				throw error
-			}
+
+				if (validation.error) {
+					const error = new createError.BadRequest(
+						'Event object failed validation',
+					)
+					handler.event.headers = { ...handler.event.headers }
+					error.details = validation.error.details
+					throw error
+				}
+			})
 			return next()
 		},
 	}
